84ddc095028861a0dabc1cdd5d3e9ab493b629dd
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-10-27 17:36:01 +0200

    #2 commit

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 from mymodule import my_func
 
 __all__ = ["unknown_func"]  # Noncompliant. "unknown_func" is undefined
+
+
+import requests
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
