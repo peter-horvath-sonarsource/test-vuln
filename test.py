a792a09efa42a90aa186c29d33a6edbe95d68719
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-11-03 13:35:59 +0100

    New vuln

--- test.py
+++ test.py
@@ -8,3 +8,16 @@ import requests
 requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
 requests.get('https://example.domain', verify=False) # Noncompliant
 
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from database.users import User
+
+@app.route('hello')
+def hello():
+    id = request.args.get("id")
+    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
+    query = SQLAlchemy().session.query(User).from_statement(stmt) # Noncompliant
+    user = query.one()
+    return "Hello %s" % user.username
+
