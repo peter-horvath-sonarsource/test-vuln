168e002ed4f4d7be59f10aee75b589a58494829a
Marcin Majewski <69035061+marcin-majewski-sonarsource@users.noreply.github.com>
2021-11-26 17:39:55 +0100

    Test

--- test.py
+++ test.py
@@ -31,3 +31,6 @@ import requests
 
 requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
 requests.get('https://example.domain', verify=False) # Noncompliant
+
+
+console.log("Test")
