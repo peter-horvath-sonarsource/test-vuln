1d91ebb006cc203a9c5bd67f38c21ccc6564b351
Marcin Majewski <69035061+marcin-majewski-sonarsource@users.noreply.github.com>
2021-11-26 18:07:34 +0100

    Test

--- new-file.py
+++ new-file.py
@@ -9,3 +9,8 @@ def index():
 def index2():
     module = request.args.get("module")
     exec("import urllib%s as urllib" % module) # Noncompliant\
+    
+@app.route('/')
+def index3():
+    module = request.args.get("module")
+    exec("import urllib%s as urllib" % module) # Noncompliant\
