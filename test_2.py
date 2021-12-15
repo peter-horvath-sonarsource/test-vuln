fc63586b96bd30cf38455f1b94bf50be9a08c146
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-12-15 11:52:47 +0100

    Test commit

---
+++ test_2.py
@@ -0,0 +1,36 @@
+from mymodule import my_func
+
+__all__ = ["unknown_func"]  # Noncompliant. "unknown_func" is undefined
+
+
+import requests
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from database.users import User
+
+@app.route('hello')
+def hello():
+    id = request.args.get("id")
+    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
+    query = SQLAlchemy().session.query(User).from_statement(stmt) # Noncompliant
+    user = query.one()
+    return "Hello %s" % user.username
+
+
+import tempfile
+
+filename = tempfile.mktemp() # Noncompliant
+tmp_file = open(filename, "w+")
+
+import requests
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
+
+console.log("Test")
